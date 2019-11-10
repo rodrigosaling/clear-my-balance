03c90625a63fd69dc2f5aaf5494f1c276424f95a
Rodrigo Saling <rodrigosaling@gmail.com>
2019-11-10 18:37:07 -0300

    It's calculating and finding the path, but still need to work on success path.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import CytoscapeComponent from 'react-cytoscapejs/src/component';
+// import CytoscapeComponent from 'react-cytoscapejs/src/component';
 import Tree from './classes/Tree';
 import './App.css';
 
@@ -7,30 +7,14 @@ const endValue = 0;
 const startValue = 2;
 const addValues = [2, 4];
 const subtractValues = [8, 10];
+// const startValue = 1.4;
+// const addValues = [6.4, 11.6, 57.6, 115.2, 172.8];
+// const subtractValues = [5, 6.4, 11.6];
 const goBelowZero = false;
-const maxLevels = 10;
+const maxLevels = 4;
+
 // let currentLevel = 1;
 // const hasFinished = false;
-
-// class Node {
-//   constructor(value, parentValue = 0) {
-//     this.value = value;
-//     this.result = value + parentValue;
-//     this.children = [];
-//   }
-//
-//   addChild(value, parent) {
-//     const child = new Node(value, parent.result);
-//     this.children.push(child);
-//   }
-// }
-//
-// class Tree {
-//   constructor(value) {
-//     const node = new Node(value);
-//     this.root = node;
-//   }
-// }
 
 const addChildren = (parent, level = 1) => {
   if (
@@ -54,67 +38,106 @@ const addChildren = (parent, level = 1) => {
   });
 };
 
+const path = [];
+let hasFoundPath = false;
+
+// eslint-disable-next-line consistent-return
+const walkTree = node => {
+  if (node.result === endValue) {
+    hasFoundPath = true;
+    return node;
+  }
+
+  if (hasFoundPath) {
+    return node;
+  }
+
+  if (!node.children.length) {
+    // console.log('dead end');
+    return false;
+  }
+
+  node.children.forEach(child => {
+    const response = walkTree(child);
+    // console.log(successPath);
+    // if (response === false) {
+    //   return successPath.pop();
+    // }
+    if (response) {
+      // we found the complete path
+      path.unshift(response);
+    }
+    return response;
+  });
+};
+
 function App() {
   const tree = new Tree(startValue);
 
   addChildren(tree.root);
 
-  // console.log(tree);
+  console.log(tree);
 
-  // console.log('ended!');
+  walkTree(tree.root, path);
 
-  const elements = [
-    // list of graph elements to start with
-    {
-      // node a
-      data: { id: 'a' },
-    },
-    {
-      // node b
-      data: { id: 'b' },
-    },
-    {
-      // edge ab
-      data: { id: 'ab', source: 'a', target: 'b' },
-    },
-  ];
+  console.log('hasFoundPath', hasFoundPath);
+  console.log(path);
 
-  const style = [
-    // the stylesheet for the graph
-    {
-      selector: 'node',
-      style: {
-        'background-color': '#666',
-        label: 'data(id)',
-      },
-    },
+  console.log('ended!');
 
-    {
-      selector: 'edge',
-      style: {
-        width: 3,
-        'line-color': '#ccc',
-        'target-arrow-color': '#ccc',
-        'target-arrow-shape': 'triangle',
-      },
-    },
-  ];
-
-  const layout = {
-    name: 'grid',
-    rows: 1,
-  };
+  // const elements = [
+  //   // list of graph elements to start with
+  //   {
+  //     data: { id: 'a' },
+  //   },
+  //   {
+  //     data: { id: 'b' },
+  //   },
+  //   {
+  //     data: { id: 'c' },
+  //   },
+  //   {
+  //     data: { id: 'ab', source: 'a', target: 'b' },
+  //   },
+  //   {
+  //     data: { id: 'ac', source: 'a', target: 'c' },
+  //   },
+  // ];
+  //
+  // const style = [
+  //   // the stylesheet for the graph
+  //   {
+  //     selector: 'node',
+  //     style: {
+  //       'background-color': '#666',
+  //       label: 'data(id)',
+  //     },
+  //   },
+  //
+  //   {
+  //     selector: 'edge',
+  //     style: {
+  //       width: 3,
+  //       'line-color': '#ccc',
+  //       'target-arrow-color': '#ccc',
+  //       'target-arrow-shape': 'triangle',
+  //     },
+  //   },
+  // ];
+  //
+  // const layout = {
+  //   name: 'breadthfirst',
+  // };
 
   return (
     <div className="App">
       <h1>Clear my Balance</h1>
-      <CytoscapeComponent
-        elements={elements}
-        stylesheet={style}
-        layout={layout}
-        style={{ width: '600px', height: '600px', background: '#eeeeee' }}
-      />
-      ;
+      {/* <CytoscapeComponent */}
+      {/*  elements={elements} */}
+      {/*  stylesheet={style} */}
+      {/*  layout={layout} */}
+      {/*  style={{ width: '600px', height: '600px', background: '#eeeeee' }} */}
+      {/* /> */};
     </div>
   );
 }
--- src/classes/Tree.js
+++ src/classes/Tree.js
@@ -3,6 +3,7 @@ import Node from './Node';
 class Tree {
   constructor(value) {
     this.root = new Node(value);
+    this.globalId = 1;
   }
 }
 
